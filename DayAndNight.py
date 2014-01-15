d932a0f6566b1b107db496708e1e705ffd80c821
Sam Carlton <sam.crltn@gmail.com>
2014-01-14 21:47:32 -0600

    Initial commit.

---
+++ DayAndNight.py
@@ -0,0 +1,13 @@
+import sublime
+import sublime_plugin
+
+
+class DayAndNightCommand(sublime_plugin.WindowCommand):
+    def run(self, time):
+        user_settings = sublime.load_settings('Preferences.sublime-settings')
+        plugin_default_settings = sublime.load_settings('DayAndNight.sublime-settings').get('day_and_night_defaults')
+
+        user_settings.set('theme', plugin_default_settings.get(str(time) + '_theme'))
+        user_settings.set('color_scheme', plugin_default_settings.get(str(time) + '_color_scheme'))
+
+        sublime.save_settings("Preferences.sublime-settings")
